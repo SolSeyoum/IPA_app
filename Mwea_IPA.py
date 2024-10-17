4c23af4b4e418aa3fca4e694dc0d83f7bf44feef
Solomon <sd.seyoum@yahoo.com>
2024-10-17 20:13:28 +0200

    Update Mwea_IPA.py

--- Mwea_IPA.py
+++ Mwea_IPA.py
@@ -72,7 +72,7 @@ st.markdown("""
 # Load data
 dfm = pd.read_csv(r'data/IPI_by_section_Mwea_Kenya.csv')
 # shp_file = r'data\Mwea.json'
-with open(r'data\Mwea.json') as response:
+with open(r'data/Mwea.json') as response:
     geo = json.load(response)
 # shdf = gpd.read_file(shp_file ,crs="EPSG:32737")
 # shdf.columns = [x.replace('_', ' ') for x in shdf.columns]
