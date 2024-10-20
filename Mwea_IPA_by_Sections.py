02a37301d1e9f249781b2ccacd15b3fcea41f8bf
Solomon <sd.seyoum@yahoo.com>
2024-10-20 11:03:54 +0200

    Update Mwea_IPA_by_Sections.py

--- Mwea_IPA_by_Sections.py
+++ Mwea_IPA_by_Sections.py
@@ -81,12 +81,21 @@ logo_wide = r'data/logo_wide.png'
 logo_small = r'data/logo_small.png'
 
 IPA_description = {
-     "beneficial fraction": "beneficial fraction (BF) is a measure of efficiency and calculated as the ratio of sum of transpiration to evapotranspiration.",
-    "crop water deficit": "crop water deficit (CWD) is measure of adequacy and calculated as the ration of seasonal evapotranspiration to potential or reference evapotranspiration",
-    "relative water deficit": "relative water deficit (RWD) is also a measure of adequacy which is 1 minus crop water deficit",
-    "total seasonal biomass production": "total seasonal biomass production (TBP) is total biomass produced in tons",
-    "seasonal yield": "seasonal yield is the yield in a season which is crop specific and calculated using the TBP and yield factors such as moisture content, harvest index, light use efficiency correction factor and above ground over total biomass production ratio (AOT)",
-    "crop water productivity": "crop water productivity (CWP) is the seasonal yield per the amount of water consumed in kg/m3"
+    "beneficial fraction": "Beneficial fraction (BF) is the ratio of the water that is consumed as transpiration\
+         compared to overall field water consumption (ETa). ${\\footnotesize BF = T_a/ET_a}$. \
+         It is a measure of the efficiency of on farm water and agronomic practices in use of water for crop growth.",
+    "crop water deficit": "crop water deficit (CWD) is measure of adequacy and calculated as the ration of seasonal\
+        evapotranspiration to potential or reference evapotranspiration ${\\footnotesize CWD= ET_a/ET_p}$",
+    "relative water deficit": "relative water deficit (RWD) is also a measure of adequacy which is 1 minus crop water\
+          deficit ${\\footnotesize RWD= 1-ET_a/ET_p}$",
+    "total seasonal biomass production": "total seasonal biomass production (TBP) is total biomass produced in tons. \
+        ${\\footnotesize TBP = NPP * 22.222) / 1000}$",
+    "seasonal yield": "seasonal yield is the yield in a season which is crop specific and calculated using \
+        the TBP and yield factors such as moisture content, harvest index, light use efficiency correction \
+            factor and above ground over total biomass production ratio (AOT) \
+                ${\\footnotesize Yiled = TBP*HI*AOT*f_c/(1-MC)}$",
+    "crop water productivity": "crop water productivity (CWP) is the seasonal yield per the amount of water \
+        consumed in ${\\footnotesize kg/m^3}$"
 
 }
 # @st.cache_data(ttl=300)
