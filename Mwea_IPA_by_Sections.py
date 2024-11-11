8066fefe8c423cac0adccdebcc43c31efa5c5a91
Solomon <sd.seyoum@yahoo.com>
2024-11-11 20:54:29 +0100

    Update Mwea_IPA_by_Sections.py

--- Mwea_IPA_by_Sections.py
+++ Mwea_IPA_by_Sections.py
@@ -69,9 +69,11 @@ img[data-testid="stLogo"] {
             height: 4.5rem;
 }
 
-#GithubIcon {
-  visibility: hidden;
-}
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
 </style>
 """, unsafe_allow_html=True)
 
