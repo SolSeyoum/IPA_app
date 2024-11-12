9868a5b74dfd185b7f86049cddfd85e11a58b51c
Solomon <sd.seyoum@yahoo.com>
2024-11-12 15:24:41 +0100

    Update Mwea_IPA_by_Sections.py

--- Mwea_IPA_by_Sections.py
+++ Mwea_IPA_by_Sections.py
@@ -72,19 +72,6 @@ img[data-testid="stLogo"] {
 </style>
 """, unsafe_allow_html=True)
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 #######################
 # Load data
 dfm = pd.read_csv(r'data/Mwea_IPA_stat_by_blocks.csv')
