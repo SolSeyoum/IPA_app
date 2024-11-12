c74ae3ee67e79ced75ce0fcb6a08b8da82175cfb
Solomon <sd.seyoum@yahoo.com>
2024-11-12 15:17:58 +0100

    Update Mwea_IPA_by_Sections.py

--- Mwea_IPA_by_Sections.py
+++ Mwea_IPA_by_Sections.py
@@ -21,56 +21,56 @@ alt.themes.enable("dark")
 
 #######################
 # CSS styling
-st.markdown("""
-<style>
+# st.markdown("""
+# <style>
 
-[data-testid="block-container"] {
-    padding-left: 2rem;
-    padding-right: 2rem;
-    padding-top: 1rem;
-    padding-bottom: 0rem;
-    margin-bottom: -7rem;
-}
+# [data-testid="block-container"] {
+#     padding-left: 2rem;
+#     padding-right: 2rem;
+#     padding-top: 1rem;
+#     padding-bottom: 0rem;
+#     margin-bottom: -7rem;
+# }
 
-[data-testid="stVerticalBlock"] {
-    padding-left: 0rem;
-    padding-right: 0rem;
-}
+# [data-testid="stVerticalBlock"] {
+#     padding-left: 0rem;
+#     padding-right: 0rem;
+# }
 
-[data-testid="stMetric"] {
-    background-color: #1c1b1b;
-    text-align: center;
-    padding: 2px 0;
-}
+# [data-testid="stMetric"] {
+#     background-color: #1c1b1b;
+#     text-align: center;
+#     padding: 2px 0;
+# }
 
-[data-testid="stMetricLabel"] {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+# [data-testid="stMetricLabel"] {
+#   display: flex;
+#   justify-content: center;
+#   align-items: center;
+# }
 
-[data-testid="stMetricDeltaIcon-Up"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
+# [data-testid="stMetricDeltaIcon-Up"] {
+#     position: relative;
+#     left: 38%;
+#     -webkit-transform: translateX(-50%);
+#     -ms-transform: translateX(-50%);
+#     transform: translateX(-50%);
+# }
 
-[data-testid="stMetricDeltaIcon-Down"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
+# [data-testid="stMetricDeltaIcon-Down"] {
+#     position: relative;
+#     left: 38%;
+#     -webkit-transform: translateX(-50%);
+#     -ms-transform: translateX(-50%);
+#     transform: translateX(-50%);
+# }
             
-img[data-testid="stLogo"] {
-            height: 4.5rem;
-}
+# img[data-testid="stLogo"] {
+#             height: 4.5rem;
+# }
 
-</style>
-""", unsafe_allow_html=True)
+# </style>
+# """, unsafe_allow_html=True)
 
 st.markdown(
     """
