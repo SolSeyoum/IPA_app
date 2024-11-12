bbf15e995ae447e15db318efc5e359ead5324526
Solomon <sd.seyoum@yahoo.com>
2024-11-12 15:25:03 +0100

    Update Mwea_IPA_by_Blocks.py

--- pages/Mwea_IPA_by_Blocks.py
+++ pages/Mwea_IPA_by_Blocks.py
@@ -20,13 +20,6 @@ alt.themes.enable("dark")
 #######################
 # CSS styling
 
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
 st.markdown("""
 <style>
 
